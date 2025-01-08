cd5fb1c79aef587449b486abdbb2b3490ed7dbea
Samuel Figueroa <samuelfs96@gmail.com>
2025-01-08 11:12:35 -0400

    feat: :sparkles: fetching data and show on screen with pagination, errors catching and skeleton loader

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,25 @@
-import { Card, HR, Modal } from "flowbite-react";
-import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { Card, HR, Modal, Pagination } from "flowbite-react";
+import { useMemo, useState } from "react";
+import { getPeople } from "../services/api";
+import { usePagination } from "../hooks/usePagination";
+import Skeleton from "../components/Skeleton";
+import ErrorPage from "./ErrorPage";
 
 const Home = () => {
   const [openModal, setOpenModal] = useState(false);
+  const { currentPage, onPageChange } = usePagination();
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["people", currentPage],
+    queryFn: () => getPeople(currentPage),
+  });
+  const getTotalPages = useMemo(() => {
+    if (!data) return 0;
+    return Math.ceil(data?.count / 10);
+  }, [data]);
+
+  if (error) return <ErrorPage error={error} />;
+
   return (
     <>
       <Modal show={openModal} onClose={() => setOpenModal(false)}>
@@ -36,28 +53,50 @@ const Home = () => {
       </div>
       <div className="bg-black py-8 container mx-auto min-[1024px]:px-24 px-16">
         <div className="grid grid-cols-4 gap-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1">
-          {Array.from(Array(10).keys()).map((it) => (
-            <Card
-              key={it}
-              className="min-h-[240px] relative border-primary bg-death-star bg-fixed custom-star-box"
-              onClick={() => setOpenModal(true)}
-            >
-              <div>
-                <h2 className="text-xl font-bold tracking-tight hover:cursor-pointer">
-                  Lorem ipsum dolor sit amet.
-                </h2>
-                <HR className="my-2" />
-              </div>
-              <div>
-                <p className="text-sm">Birth year: Lorem, ipsum.</p>
-                <p className="text-sm">Height: Lorem, ipsum.</p>
-                <p className="text-sm">Mass: Lorem, ipsum.</p>
-                <p className="text-sm">Hair color: Lorem, ipsum.</p>
-                <p className="text-sm">Skin color: Lorem, ipsum.</p>
-                <p className="text-sm">Eye color: Lorem, ipsum.</p>
-              </div>
-            </Card>
-          ))}
+          {isLoading
+            ? Array.from(Array(10).keys()).map((item) => (
+                <Skeleton key={item} height="240px" />
+              ))
+            : data?.results?.map((character) => (
+                <Card
+                  key={character.name}
+                  className="min-h-[240px] relative border-primary bg-death-star bg-fixed custom-star-box"
+                  onClick={() => setOpenModal(true)}
+                >
+                  <div>
+                    <h2 className="text-xl font-bold tracking-tight hover:cursor-pointer">
+                      {character.name}
+                    </h2>
+                    <HR className="my-2" />
+                  </div>
+                  <div>
+                    <p className="text-sm">
+                      Birth year: {character.birth_year}
+                    </p>
+                    <p className="text-sm">Height: {character.height}</p>
+                    <p className="text-sm">Mass: {character.mass}</p>
+                    <p className="text-sm">
+                      Hair color: {character.hair_color}
+                    </p>
+                    <p className="text-sm">
+                      Skin color: {character.skin_color}
+                    </p>
+                    <p className="text-sm">Eye color: {character.eye_color}</p>
+                  </div>
+                </Card>
+              ))}
+        </div>
+        <div className="custom-pagination">
+          <hr className="mt-8 mb-8" />
+          <div className="flex justify-center">
+            <Pagination
+              className="text-center"
+              currentPage={currentPage}
+              onPageChange={onPageChange}
+              showIcons={true}
+              totalPages={getTotalPages}
+            />
+          </div>
         </div>
       </div>
     </>
