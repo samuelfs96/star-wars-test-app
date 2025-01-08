07697dfc81d9367762d9e2d530754f8a1ad66fdb
Samuel Figueroa <samuelfs96@gmail.com>
2025-01-08 09:40:23 -0400

    feat: :sparkles: add layout wrapper for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Button } from 'flowbite-react'
-import './App.css'
+import { Button } from "flowbite-react";
+import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <Button>Click me</Button>;
+      <Layout>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Button>Click me</Button>
+      </Layout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
